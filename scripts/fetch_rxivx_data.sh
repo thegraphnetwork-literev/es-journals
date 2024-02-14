81a39b591fdbd6040b61dcd4eccbea05341e872b
Sandro Loch <es.loch@gmail.com>
2024-02-14 13:41:39 -0300

    chore: Add initial data for json files

--- scripts/fetch_rxivx_data.sh
+++ scripts/fetch_rxivx_data.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-# Activate conda enviroment
-source /opt/miniforge/bin/activate rxiv-rest-api
+# Activate conda environment
+eval "$(conda shell.bash hook)"
+conda activate rxiv-rest-api-server
 
 # Capture the current working directory
 path_root=$(pwd)
@@ -11,21 +12,20 @@ path_root=$(pwd)
 # Get the server name from the command-line argument
 server=$1
 
-most_recent_file=$(ls -1 ${path_root}/src/data/rxivx/${server}/downloaded | sort -r | head -n 1)
+most_recent_file=$(ls -1 ${path_root}/data/rxivx/${server}/downloaded | sort -r | head -n 1)
 latest_date=$(echo $most_recent_file | grep -oE '[0-9]{4}-[0-9]{2}-[0-9]{2}' | tail -1)
 current_date=$(date +'%Y-%m-%d')
 output_filename="${server}_${latest_date}_${current_date}.json"
 
-
 # Execute the download process
-if makim scheduler.download-rxivr --server ${server} --begin ${latest_date} --end ${current_date} --target ${path_root}/src/data/rxivx/${server}/downloaded/ ; then
+if makim scheduler.download-rxivr --server ${server} --begin ${latest_date} --end ${current_date} --target ${path_root}/data/rxivx/${server}/downloaded/ ; then
     echo "[II] INFO: The latest database file generated is ${output_filename}"
-    if python ${path_root}/src/merge_arxiv_data.py "${path_root}/src/data/rxivx/${server}/final/${server}_full_data.json" "src/data/rxivx/${server}/downloaded/${output_filename}"; then
+    if python ${path_root}/scripts/merge_arxiv_data.py "${path_root}/data/rxivx/${server}/final/${server}_full_data.json" "data/rxivx/${server}/downloaded/${output_filename}"; then
         echo "[II] INFO: Merge in the full database completed successfully."
 
         # If both download and merge were successful, delete the most recent file
         echo "[II] INFO: Deleting the most old file: ${most_recent_file}"
-        rm "${path_root}/src/data/rxivx/${server}/downloaded/${most_recent_file}"
+        rm "${path_root}/data/rxivx/${server}/downloaded/${most_recent_file}"
     else
         echo "[II] ERROR: Merge process failed."
         exit 1
