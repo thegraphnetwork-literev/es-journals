ec127058033b2dd0d2a07d8b391488892ee43f6e
Sandro Loch <es.loch@gmail.com>
2024-02-21 15:39:41 -0300

    feat: Implement CRON tasks for Elasticsearch data fetching and indexing, and Add NGINX deployment services (#1)

---
+++ scripts/fetch_rxivx_data.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+source /opt/miniforge/bin/activate rxiv-rest-api
+
+path_root=$(pwd)
+
+server="$1"
+
+# Find the most recent file and construct filenames based on dates
+most_recent_file=$(ls -1 "${path_root}/data/rxivx/${server}/downloaded" | sort -r | head -n 1)
+latest_date=$(echo "$most_recent_file" | grep -oE '[0-9]{4}-[0-9]{2}-[0-9]{2}' | tail -1)
+current_date=$(date +'%Y-%m-%d')
+output_filename="${server}_${latest_date}_${current_date}.json"
+
+# Execute the download process
+if makim scheduler.download-rxivr --server "${server}" --begin "${latest_date}" --end "${current_date}" --target "${path_root}/data/rxivx/${server}/downloaded/" ; then
+    echo "[INFO]: The latest database file generated is ${output_filename}"
+    if python "${path_root}/scripts/merge_arxiv_data.py" "${path_root}/data/rxivx/${server}/final/${server}_full_data.json" "${path_root}/data/rxivx/${server}/downloaded/${output_filename}"; then
+        echo "[INFO]: Merge in the full database completed successfully."
+
+        # If both download and merge were successful, consider handling of files as needed
+        echo "[INFO]: Handling of files post-merge can be placed here."
+    else
+        echo "[ERROR]: Merge process failed."
+        exit 1
+    fi
+    echo "[SUCCESS]: All processes completed successfully."
+else
+    echo "[ERROR]: Download process failed."
+    exit 1
+fi
---
+++ scripts/index_arxiv_to_es.py
@@ -0,0 +1,108 @@
+import json
+import os
+import time
+from pathlib import Path
+from typing import Optional
+
+import typer
+from dotenv import load_dotenv
+from elasticsearch import Elasticsearch
+from loguru import logger
+
+load_dotenv()
+
+app = typer.Typer()
+
+
+index_name_placeholder = "index_name"
+log_filename = (
+    f"/tmp/elasticrxivx_{index_name_placeholder}_{time.strftime('%Y%m%d-%H%M%S')}.log"
+)
+
+ES_HOST = os.getenv("ES_HOST", "https://localhost:9200")
+ES_USERNAME = os.getenv("ES_USERNAME", "elastic")
+ES_PASSWORD = os.getenv("ES_PASSWORD", "")
+
+
+def create_es_client(host: str, username: str, password: str) -> Elasticsearch:
+    """
+    Creates an Elasticsearch client using the provided credentials.
+    """
+    return Elasticsearch([host], basic_auth=(username, password))
+
+
+def index_json_data(es_client: Elasticsearch, file_path: str, index_name: str) -> None:
+    """
+    Reads data from a JSON file and indexes it into Elasticsearch.
+    """
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+
+        successful_docs = 0
+        for doc in data:
+            try:
+                res = es_client.index(index=index_name, document=doc)
+                successful_docs += 1
+            except Exception as doc_error:
+                logger.error(f"Failed to index document: {doc_error}")
+
+        logger.info(
+            f"Total documents indexed successfully: {successful_docs}/{len(data)}"
+        )
+    except Exception as e:
+        logger.error(f"Failed to index data for {index_name}: {e}")
+
+
+def find_arxiv_path(index_name: str) -> Optional[str]:
+    """
+    Constructs the absolute file path for the specified database name.
+    """
+    if index_name:
+        base_dir = Path(__file__).resolve().parent.parent
+        file_path = (
+            base_dir / f"data/rxivx/{index_name}/final/{index_name}_full_data.json"
+        )
+        return str(file_path)
+    else:
+        logger.info("No index name provided.")
+        return None
+
+
+def validate_index_name(index_name: str):
+    valid_indices = ["biorxiv", "medrxiv"]
+    if index_name not in valid_indices:
+        logger.error(
+            f"Invalid index name: {index_name}. Valid options are 'biorxiv' or 'medrxiv'."
+        )
+        raise typer.BadParameter(
+            f"Invalid index name: {index_name}. Valid options are 'biorxiv' or 'medrxiv'."
+        )
+    return index_name
+
+
+@app.command()
+def main(index_name: str = typer.Argument(..., callback=validate_index_name)):
+    global log_filename
+    log_filename = log_filename.replace(index_name_placeholder, index_name)
+    logger.add(log_filename, rotation="10 MB", retention="10 days", level="INFO")
+
+    logger.info("Script started.")
+    start_time = time.time()
+    logger.info(f"Starting indexing for {index_name}")
+
+    es_client = create_es_client(ES_HOST, ES_USERNAME, ES_PASSWORD)
+    file_path = find_arxiv_path(index_name)
+    if file_path:
+        index_json_data(es_client, file_path, index_name)
+        end_time = time.time()
+        total_time = end_time - start_time
+        logger.info(
+            f"Completed indexing for {index_name}. Total time: {total_time:.2f} seconds"
+        )
+    else:
+        logger.info(f"No data indexed for {index_name}.")
+
+
+if __name__ == "__main__":
+    app()
