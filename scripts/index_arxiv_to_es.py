2fab90c05ec5ec2a6bd472f22f839a2dc6e6f733
Sandro Loch <es.loch@gmail.com>
2024-02-26 12:42:10 +0000

    feat(indexing): Enhance indexing process to only include latest data, preventing duplication in Elasticsearch

--- scripts/index_arxiv_to_es.py
+++ scripts/index_arxiv_to_es.py
@@ -2,18 +2,15 @@ import json
 import os
 import time
 from pathlib import Path
-from typing import Optional
 import hashlib
-
 import typer
 from dotenv import load_dotenv
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, exceptions
 from loguru import logger
 
 load_dotenv()
 
 app = typer.Typer()
-
 
 index_name_placeholder = "index_name"
 log_filename = (
@@ -26,65 +23,53 @@ ES_PASSWORD = os.getenv("ES_PASSWORD", "")
 
 
 def create_es_client(host: str, username: str, password: str) -> Elasticsearch:
-    """
-    Creates an Elasticsearch client using the provided credentials.
-    """
-    return Elasticsearch([host], basic_auth=(username, password))
+    return Elasticsearch([host], basic_auth=(username, password), verify_certs=False)
+
 
 def generate_document_id(doc: dict) -> str:
-    """
-    Generates a unique ID for a document.
-    This example uses a hash of the document's content, but you can modify
-    it to use other unique fields from your document.
-    """
     doc_string = json.dumps(doc, sort_keys=True)
-    return hashlib.sha256(doc_string.encode('utf-8')).hexdigest()
+    return hashlib.sha256(doc_string.encode("utf-8")).hexdigest()
+
+
+def document_exists(es_client: Elasticsearch, index_name: str, doc_id: str) -> bool:
+    try:
+        return es_client.exists(index=index_name, id=doc_id)
+    except exceptions.NotFoundError:
+        return False
+
 
 def index_json_data(es_client: Elasticsearch, file_path: str, index_name: str) -> None:
-    """
-    Reads data from a JSON file and indexes it into Elasticsearch,
-    using a unique ID for each document to avoid duplicates.
-    """
     try:
         with open(file_path, "r", encoding="utf-8") as file:
             data = json.load(file)
 
         successful_docs = 0
         for doc in data:
-            doc_id = generate_document_id(doc)  # Generate a unique ID for the document
-            try:
-                res = es_client.index(index=index_name, id=doc_id, document=doc)
-                successful_docs += 1
-            except Exception as doc_error:
-                logger.error(f"Failed to index document: {doc_error}")
+            doc_id = generate_document_id(doc)
+            if not document_exists(es_client, index_name, doc_id):
+                try:
+                    res = es_client.index(index=index_name, id=doc_id, document=doc)
+                    successful_docs += 1
+                except Exception as doc_error:
+                    logger.error(f"Failed to index document: {doc_error}")
+            else:
+                logger.info(f"Document with ID {doc_id} already exists. Skipping.")
 
         logger.info(
-            f"Total documents indexed successfully: {successful_docs}/{len(data)}"
+            f"Total new documents indexed successfully: {successful_docs}/{len(data)}"
         )
     except Exception as e:
         logger.error(f"Failed to index data for {index_name}: {e}")
 
-def find_arxiv_path(index_name: str) -> Optional[str]:
-    """
-    Constructs the absolute file path for the specified database name.
-    """
-    if index_name:
-        base_dir = Path(__file__).resolve().parent.parent
-        file_path = (
-            base_dir / f"data/rxivx/{index_name}/final/{index_name}_full_data.json"
-        )
-        return str(file_path)
-    else:
-        logger.info("No index name provided.")
-        return None
+
+def find_arxiv_path(index_name: str) -> Path:
+    base_dir = Path(__file__).resolve().parent.parent
+    return base_dir / f"data/rxivx/{index_name}/final/{index_name}_full_data.json"
 
 
 def validate_index_name(index_name: str):
     valid_indices = ["biorxiv", "medrxiv"]
     if index_name not in valid_indices:
-        logger.error(
-            f"Invalid index name: {index_name}. Valid options are 'biorxiv' or 'medrxiv'."
-        )
         raise typer.BadParameter(
             f"Invalid index name: {index_name}. Valid options are 'biorxiv' or 'medrxiv'."
         )
@@ -98,20 +83,10 @@ def main(index_name: str = typer.Argument(..., callback=validate_index_name)):
     logger.add(log_filename, rotation="10 MB", retention="10 days", level="INFO")
 
     logger.info("Script started.")
-    start_time = time.time()
-    logger.info(f"Starting indexing for {index_name}")
-
     es_client = create_es_client(ES_HOST, ES_USERNAME, ES_PASSWORD)
     file_path = find_arxiv_path(index_name)
-    if file_path:
-        index_json_data(es_client, file_path, index_name)
-        end_time = time.time()
-        total_time = end_time - start_time
-        logger.info(
-            f"Completed indexing for {index_name}. Total time: {total_time:.2f} seconds"
-        )
-    else:
-        logger.info(f"No data indexed for {index_name}.")
+    index_json_data(es_client, file_path, index_name)
+    logger.info(f"Completed indexing for {index_name}.")
 
 
 if __name__ == "__main__":
