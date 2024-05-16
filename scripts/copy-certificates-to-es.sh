34f3b81bdaf8b072de75ace99ea73c18bdb6a361
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2024-05-16 11:29:28 -0400

    add initial script for copying certs

--- scripts/copy-certificates-to-es.sh
+++ scripts/copy-certificates-to-es.sh
@@ -2,5 +2,5 @@
 
 CERT_PATH=/etc/letsencrypt/archives/${CERTBOT_DOMAIN}
 
-sugar exec nginx --cmd ls -1 ${CERT_PATH} | grep 'privkey' | sort -V | tail -n 1
-sugar exec nginx --cmd ls -1 ${CERT_PATH} | grep 'cert' | sort -V | tail -n 1
+sugar exec --service nginx --cmd ls -1 ${CERT_PATH} | grep 'privkey' | sort -V | tail -n 1
+sugar exec --service nginx --cmd ls -1 ${CERT_PATH} | grep 'cert' | sort -V | tail -n 1
